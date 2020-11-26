1782db9fa17d8e84d420779fccfa4a619dc6e52d
Shuguang LYU <scysl4@nottingham.edu.cn>
2020-11-26 23:45:22 +0800

    javadoc relocated

--- Sokoban/src/main/Modal/History.java
+++ Sokoban/src/main/Modal/History.java
@@ -5,15 +5,32 @@ import Controller.GameEngine;
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * The type History.
+ */
 public final class History {
+    /**
+     * The constant historyList.
+     */
     private static Stack<Level> historyList = new Stack<>();
 
+    /**
+     * Instantiates a new History.
+     */
     private History() {}
 
+    /**
+     * Gets history.
+     *
+     * @return the history
+     */
     public static Stack<Level> getHistory() {
         return historyList;
     }
 
+    /**
+     * Trace history.
+     */
     public static void traceHistory() {
         Level previousLevel;
         if (!historyList.isEmpty()) {
@@ -22,6 +39,9 @@ public final class History {
         }
     }
 
+    /**
+     * Reset history.
+     */
     public static void resetHistory() {
         Level previousLevel;
         if (!historyList.isEmpty()) {
